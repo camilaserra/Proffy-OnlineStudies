d3a48fa0ecb6a48768e57f1bebcbf71001d2eba9
Camila Serra <camilaserrasilva@gmail.com>
2020-08-05 22:22:05 -0300

    update 1

---
+++ scripts/addField.js
@@ -0,0 +1,14 @@
+//procurar o botão
+document.querySelector("add-time")
+.addEventListener('click', cloneField)
+//quando clicar no botão
+
+//executar uma ação
+function cloneField() {
+    console.log("cheguei aqui")
+}
+//duplicar os campos
+
+//colocar na pagina
+
+
