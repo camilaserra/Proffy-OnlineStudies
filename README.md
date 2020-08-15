84fa77b6bf22768e7cbbbaa86cb01f1992da4759
Camila Serra <camilaserrasilva@gmail.com>
2020-08-14 21:51:09 -0300

    update 5

---
+++ README.md
@@ -0,0 +1,1 @@
+  <img src="https://raw.githubusercontent.com/camilaserra/Proffy-OnlineStudies/a0693f1dda57096e175596921579e7f7f46d3a42/proffy.jpg" width="700" >
--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -14,7 +14,7 @@ const proffys = [
     },
   
     { 
-      name:"Camila Serra", 
+      name:"Wilson Montelo", 
       avatar: "https://avatars1.githubusercontent.com/u/66450896?s=460&u=186f4616de4290975d1dd5722f6a293c837ee6f2&v=4", 
       whatsapp: "99999999999", 
       bio: "Hi, I'm Wilson and I work as a Front-end developer.<br><br>I participated in the development of several digital products,development specifically, task automation, software architecture,responsive design and html semantics.", 
@@ -27,9 +27,9 @@ const proffys = [
   
     { 
       name:"Victor Hugo", 
-      avatar: "https://avatars1.githubusercontent.com/u/66450896?s=460&u=186f4616de4290975d1dd5722f6a293c837ee6f2&v=4", 
+      avatar: "https://media-exp1.licdn.com/dms/image/C4E03AQHx7oVUxfo8pg/profile-displayphoto-shrink_100_100/0?e=1602720000&v=beta&t=EUnFYsuUBe_COCDtr0RtL8DDzk9BSBOVrA5nWUMspQs", 
       whatsapp: "99999999999", 
-      bio: "Hi, I'm Wilson and I work as a Front-end developer.<br><br>I participated in the development of several digital products,development specifically, task automation, software architecture,responsive design and html semantics.", 
+      bio: "Hi, I'm Victor.<br><br>I participated in the development of several digital products,development specifically, task automation, software architecture,responsive design and html semantics.", 
       subject: "Alemão", 
       cost:"80", 
       weekday: [0], 
